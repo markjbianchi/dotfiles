89cc2e05ace86bd0a12b3936bd0990cc381f6233
Mark Bianchi <markjbianchi@gmail.com>
2021-03-31 15:38:20 -0600

    Improved vim-update script to be able to add or update based on cmdline flag.

--- vim-upd.sh
+++ vim-upd.sh
@@ -1,20 +1,24 @@
 #! /usr/bin/env bash
 
+PACK=$HOME/.vim/pack/myplugins/start
+
+op=pull
+if [ $1 == "-a" ]; then
+  op=add
+fi
+
 # Execute these commands once to create a subtree of the desired plugin
-git subtree add --prefix vim/pack/myplugins/start/vim-airline https://github.com/vim-airline/vim-airline.git master --squash
-git subtree add --prefix vim/pack/myplugins/start/vim-airline-themes https://github.com/vim-airline/vim-airline-themes.git master --squash
+git subtree $op --prefix $PACK/vim-airline https://github.com/vim-airline/vim-airline.git master --squash
+git subtree $op --prefix $PACK/vim-airline-themes https://github.com/vim-airline/vim-airline-themes.git master --squash
 
-git subtree add --prefix vim/pack/myplugins/start/surround https://github.com/tpope/vim-surround.git master --squash
-git subtree add --prefix vim/pack/myplugins/start/commentary https://github.com/tpope/vim-commentary.git master --squash
-#git subtree add --prefix .vim/pack/myplugins/start/sensible https://github.com/tpope/vim-sensible.git master --squash
+git subtree $op --prefix $PACK/surround https://github.com/tpope/vim-surround.git master --squash
+git subtree $op --prefix $PACK/commentary https://github.com/tpope/vim-commentary.git master --squash
+#git subtree $op --prefix $PACK/sensible https://github.com/tpope/vim-sensible.git master --squash
 
-git subtree add --prefix vim/pack/myplugins/start/vim-better-whitespace https://github.com/ntpeters/vim-better-whitespace.git master --squash
-git subtree add --prefix vim/pack/myplugins/start/onedark https://github.com/joshdick/onedark.git master --squash
-git subtree add --prefix vim/pack/myplugins/start/a.vim https://github.com/vim-scripts/a.vim.git master --squash
-git subtree add --prefix vim/pack/myplugins/start/goyo.vim https://github.com/junegunn/goyo.vim.git master --squash
-git subtree add --prefix vim/pack/myplugins/start/vim-sneak https://github.com/justinmk/vim-sneak.git master --squash
-git subtree add --prefix vim/pack/myplugins/start/bufexplorer https://github.com/jlanzarotta/bufexplorer.git master --squash
+git subtree $op --prefix $PACK/a.vim https://github.com/vim-scripts/a.vim.git master --squash
+git subtree $op --prefix $PACK/bufexplorer https://github.com/jlanzarotta/bufexplorer.git master --squash
+git subtree $op --prefix $PACK/vim-better-whitespace https://github.com/ntpeters/vim-better-whitespace.git master --squash
+git subtree $op --prefix $PACK/onedark https://github.com/ukyouz/onedark.git master --squash
+git subtree $op --prefix $PACK/goyo.vim https://github.com/junegunn/goyo.vim.git master --squash
+git subtree $op --prefix $PACK/vim-sneak https://github.com/justinmk/vim-sneak.git master --squash
 
-
-
-# These commands will update (pull) from the subtree repos
